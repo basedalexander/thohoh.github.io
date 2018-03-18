b89a37cc908cefa56bb9603fc38567ec9b2f7876
thohoh <itrefak@gmail.com>
2018-03-18 20:23:41 +0200

    updated cv

---
+++ markup-components.js
@@ -0,0 +1,79 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const button_plugin_markup_component_1 = require("./button/button-plugin.markup-component");
+const container_plugin_markup_component_1 = require("./container/container-plugin.markup-component");
+const expansion_panel_plugin_markup_component_1 = require("./expansion-panel/expansion-panel-plugin.markup-component");
+const formlayout_plugin_markup_component_1 = require("./formlayout/formlayout-plugin.markup-component");
+const horizontal_splitter_markup_component_1 = require("./horizontal-splitter/horizontal-splitter.markup-component");
+const input_check_plugin_markup_component_1 = require("./input-check/input-check-plugin.markup-component");
+const input_file_plugin_markup_component_1 = require("./input-file/input-file-plugin.markup-component");
+const input_number_plugin_markup_component_1 = require("./input-number/input-number-plugin.markup-component");
+const input_radio_plugin_markup_component_1 = require("./input-radio/input-radio-plugin.markup-component");
+const input_select_plugin_markup_component_1 = require("./input-select/input-select-plugin.markup-component");
+const input_slide_toggle_plugin_markup_component_1 = require("./input-slide-toggle/input-slide-toggle-plugin.markup-component");
+const input_string_plugin_markup_component_1 = require("./input-string/input-string-plugin.markup-component");
+const label_plugin_markup_component_1 = require("./label/label-plugin.markup-component");
+const list_view_plugin_markup_component_1 = require("./list-view/list-view-plugin.markup-component");
+const static_layout_plugin_markup_component_1 = require("./static-layout/static-layout-plugin.markup-component");
+const tab_control_plugin_markup_component_1 = require("./tab-control/tab-control-plugin.markup-component");
+const tab_control_item_markup_component_1 = require("./tab-control/tab-control-item.markup-component");
+const text_plugin_markup_component_1 = require("./text/text-plugin.markup-component");
+const form_host_markup_component_1 = require("./form-host/form-host.markup-component");
+const pagination_plugin_markup_component_1 = require("./pagination/pagination-plugin.markup-component");
+const filter_plugin_markup_component_1 = require("./filter/filter-plugin.markup-component");
+const adaptive_menu_markup_component_1 = require("./adaptive-menu/adaptive-menu.markup-component");
+const user_markup_component_1 = require("./user/user.markup-component");
+const image_plugin_markup_component_1 = require("./image/image-plugin.markup-component");
+const chart_plugin_markup_component_1 = require("./chart/chart-plugin.markup-component");
+const datagrid_markup_component_1 = require("./datagrid/datagrid.markup-component");
+const form_host_modal_markup_component_1 = require("./form-host-modal/form-host-modal.markup-component");
+const reference_plugin_markup_component_1 = require("./reference/reference-plugin.markup-component");
+const hyperlink_plugin_markup_component_1 = require("./hyperlink/hyperlink-plugin.markup-component");
+const data_select_plugin_markup_component_1 = require("./data-select/data-select-plugin.markup-component");
+const workflow_markup_component_1 = require("./workflow/workflow.markup-component");
+const file_upload_plugin_markup_component_1 = require("./file-upload/file-upload-plugin.markup-component");
+const datepicker_plugin_markup_component_1 = require("./datepicker/datepicker-plugin.markup-component");
+const tab_sheet_control_plugin_markup_component_1 = require("./tab-sheet-control/tab-sheet-control-plugin.markup-component");
+const table_data_view_markup_component_1 = require("./table-view/table-view-data/table-data-view.markup-component");
+exports.MODEL_BUILDER_COMPONENTS = [
+    button_plugin_markup_component_1.ButtonMarkupPluginMarkupComponent,
+    image_plugin_markup_component_1.ImageMarkupPluginMarkupComponent,
+    chart_plugin_markup_component_1.ChartluginMarkupComponent,
+    datagrid_markup_component_1.DatagridPluginMarkupComponent,
+    container_plugin_markup_component_1.ContainerPluginMarkupComponent,
+    expansion_panel_plugin_markup_component_1.ExpansionPanelPluginMarkupComponent,
+    formlayout_plugin_markup_component_1.FormlayoutPluginMarkupComponent,
+    horizontal_splitter_markup_component_1.HorizontalSplitterPluginMarkupComponent,
+    input_check_plugin_markup_component_1.InputCheckPluginMarkupComponent,
+    input_file_plugin_markup_component_1.InputFilePluginMarkupComponent,
+    input_number_plugin_markup_component_1.InputNumberPluginMarkupComponent,
+    input_radio_plugin_markup_component_1.InputRadioPluginMarkupComponent,
+    input_select_plugin_markup_component_1.InputSelectPluginMarkupComponent,
+    input_slide_toggle_plugin_markup_component_1.InputSlideTogglePluginMarkupComponent,
+    input_string_plugin_markup_component_1.InputStringPluginMarkupComponent,
+    label_plugin_markup_component_1.LabelMarkupPluginMarkupComponent,
+    list_view_plugin_markup_component_1.ListViewPluginMarkupComponent,
+    static_layout_plugin_markup_component_1.StaticLayoutPluginMarkupComponent,
+    tab_control_plugin_markup_component_1.TabControlPluginMarkupComponent,
+    tab_sheet_control_plugin_markup_component_1.TabSheetControlPluginMarkupComponent,
+    tab_control_item_markup_component_1.TabControlItemMarkupComponent,
+    input_file_plugin_markup_component_1.InputFilePluginMarkupComponent,
+    horizontal_splitter_markup_component_1.HorizontalSplitterPluginMarkupComponent,
+    table_data_view_markup_component_1.TableDataViewPluginMarkupComponent,
+    table_view_markup_component_1.TableViewPluginMarkupComponent,
+    pagination_plugin_markup_component_1.PaginationPluginMarkupComponent,
+    filter_plugin_markup_component_1.FilterPluginMarkupComponent,
+    text_plugin_markup_component_1.TextMarkupPluginMarkupComponent,
+    form_host_markup_component_1.FormHostPluginMarkupComponent,
+    form_host_modal_markup_component_1.FormHostModalPluginMarkupComponent,
+    adaptive_menu_markup_component_1.AdaptiveMenuPluginMarkupComponent,
+    user_markup_component_1.UserPluginMarkupComponent,
+    reference_plugin_markup_component_1.ReferencePluginMarkupComponent,
+    hyperlink_plugin_markup_component_1.HyperlinkPluginMarkupComponent,
+    data_select_plugin_markup_component_1.DataSelectPluginMarkupComponent,
+    workflow_markup_component_1.WorkflowPluginMarkupComponent,
+    file_upload_plugin_markup_component_1.FileUploadPluginMarkupComponent,
+    datepicker_plugin_markup_component_1.DatepickerPluginMarkupComponent
+];
+const table_view_markup_component_1 = require("./table-view/table-view/table-view.markup-component");
+//# sourceMappingURL=markup-components.js.map
